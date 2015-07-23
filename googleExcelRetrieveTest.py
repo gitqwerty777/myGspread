6a845eb3a32dcf15eb98b7256dccf9c22645b2f9
gitqwerty777 <goohcl777@gmail.com>
2015-07-23 12:46:37 +0800

    refactor, add API, add DownloadFile

--- googleExcelRetrieveTest.py
+++ googleExcelRetrieveTest.py
@@ -8,72 +8,43 @@ from types import *
 import csv
 # http://gspread.readthedocs.org/en/latest/oauth2.html
 # http://stackoverflow.com/questions/20585218/install-python-package-without-root-access 
+# cannot fail-string
+# TODO: exe file in windows, downloadFIle and rename
+#https://docs.google.com/spreadsheets/d/13Y8EbTCLtuBJYQyQ_eXwveac3diD4ToGXpUsMfVSgHw/edit#gid=0
 
-logging.basicConfig(filename='questionGetter.log',
-                            filemode='a',
-                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.WARNING)
-logger = logging.getLogger('QuestionGetter')
-
-json_key = json.load(open('googleDocsRetrieveTest-9f78e34059f7.json'))
-scope = ['https://spreadsheets.google.com/feeds']
-
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
-gc = gspread.authorize(credentials)
-
-# Login with your Google account(not provided now...)
-# Open a worksheet from spreadsheet with one shot
-worksheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/13Y8EbTCLtuBJYQyQ_eXwveac3diD4ToGXpUsMfVSgHw/edit?usp=sharing").sheet1
-#worksheet = gc.open("nature").sheet(1);
-
-valueList = worksheet.get_all_values()
-#print valueList
-
-# Transform to CSV Format
-f = open("Questions.csv", "wb")
-writer = csv.writer(f)
-
-titleList = valueList[0][:]
-strtypeList = valueList[1][:]
-valueListWithoutTitle = valueList[:]
-valueListWithoutTitle.pop(0) # remove title
-valueListWithoutTitle.pop(0) # remove type
-
-LINKCOUNT = 0
 class LinkFactory:
     def createLink(self, s):
         if s == "None":
             return EmptyLink() 
         else:
             return RealLink(s)
+            
 class RealLink:
+    LINKCOUNT = 0 # static number
     def __init__(self, s):
         self.link = s
     def isLink(self):
         try:
             ret = urllib.urlopen(self.link)
         except:
-            print "not link1"
             return False
         else:
             if ret.code == 200:
                 return True
             else:
-                print "not link"
                 return False
                 
-    def downloadFileFromLink():
-        downloadFile = urllib.URLopener()
-        localFilename = "%04d." % LINKCOUNT + link.split(".")[-1]
-        LINKCOUNT += 1
-        print "local file name = %s" % localFilename
+    def downloadFile(self):
         try:
-            downloadFile.retrieve(link, localFilename)
-        except IOError:
+            f = urllib.URLopener()
+            localFilename = "%05d." % RealLink.LINKCOUNT + self.link.split(".")[-1]
+            f.retrieve(self.link, localFilename)
+            RealLink.LINKCOUNT += 1
+        except:
+            logger.error("downloadFile Error")
             return ""
         return localFilename
-            #if() # connect available    
+            
 class EmptyLink:
     def __init__(self):
         self.link = ""
@@ -83,18 +54,6 @@ class EmptyLink:
     def downloadFile(self):
         pass
             
-typeList = []
-for elementType in strtypeList:
-    if elementType == "int":
-        typeList.append(IntType)
-    elif elementType == "string":
-        typeList.append(StringType)
-    elif elementType == "float":
-        typeList.append(FloatType)
-    elif elementType == "link":
-        typeList.append(InstanceType)
-#print "typelist = ", str(typeList)
-
 class LinkNotFoundError(Exception):
     def __init__(self, message, errors):
         # Call the base class constructor with the parameters it needs
@@ -103,37 +62,102 @@ class LinkNotFoundError(Exception):
         self.errors = errors
 
 linkFactory = LinkFactory()
-def checkTypeError(row):
-    for i, element in enumerate(row):
-        try:
-            if(typeList[i] == InstanceType):
-                mylink = linkFactory.createLink(element)
-                if not mylink.isLink():
-                    raise LinkNotFoundError("link not found", 111)
-            else:
-                element = typeList[i](element)
-        except LinkNotFoundError:
-            logger.warning("row %s error:link not found" % str(row))
-            return False
-        except NameError:
-            logger.warning("row %s error:type of %s(type = %s) not equal to %s" % (str(row), str(element), type(element), typeList[i]))
-            valueListWithoutTitle.remove(row) #check
-            return False
+
+class QuestionDownloader:
+    def __init__(self, jsonKeyFile, questionURL):
+        gc = self.openjsonKey(jsonKeyFile)
+        spreadSheet = gc.open_by_url(questionURL)
+        self.workSheets = spreadSheet.worksheets()
         
-    return True
+    def openjsonKey(self, jsonKeyFile):
+        json_key = json.load(open(jsonKeyFile))
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+        gc = gspread.authorize(credentials)
+        return gc
 
-valueListWithoutTitle = [row for row in valueListWithoutTitle if checkTypeError(row)]
+    def SaveAllSheetsinJSON(self):
+        for i in range(len(self.worksheets)):
+            self.SaveSheetinJSON(i)
+
+    def SaveSheetinJSON(self, sheetIndex): # Transform CSV to json
+        self.LoadWorkSheet(sheetIndex)
+        self.SaveSheetinCSV()
+        f = open("Questions.csv", 'r')
+        csv_reader = csv.DictReader(f,self.titleList)
+        jsonf = open("Question%d.json" % sheetIndex,'w')
+        data = json.dumps([r for r in csv_reader], indent=4, separators=(',', ': '))
+        jsonf.write(data)
+        f.close()
+        jsonf.close()        
+        
+    def SaveSheetinCSV(self):
+        f = open("Questions.csv", "wb")
+        writer = csv.writer(f)
+        writer.writerows(self.valueList)
+        f.close()
+
+    def LoadWorkSheet(self, sheetIndex):
+        self.workSheet = self.workSheets[sheetIndex]
+        totalList = self.workSheet.get_all_values()
+
+        self.titleList = totalList[0][:]
+        strtypeList = totalList[1][:]
+        totalList.pop(0) # remove title
+        totalList.pop(0) # remove type
+
+        self.typeList = self.LoadType(strtypeList)
+        self.valueList = [row for row in totalList if self.checkTypeError(row)]
+
+    def LoadType(self, strtypeList):
+        typeList = []
+        for elementType in strtypeList:
+            if elementType == "int":
+                typeList.append(IntType)
+            elif elementType == "string":
+                typeList.append(StringType)
+            elif elementType == "float":
+                typeList.append(FloatType)
+            elif elementType == "link":
+                typeList.append(InstanceType)
+        print "typelist = ", str(typeList)
+        return typeList
+
+    def checkTypeError(self, row):
+        for i, element in enumerate(row):
+            try:
+                if(self.typeList[i] == InstanceType):
+                    mylink = linkFactory.createLink(element)
+                    if not mylink.isLink():
+                        raise LinkNotFoundError("link not found", 111)
+                    else:
+                        newfilename = mylink.downloadFile()
+                        row[i] = newfilename # ???
+                else:
+                    element = self.typeList[i](element)
+            except LinkNotFoundError:
+                logger.error("%s error: link not found" % (str(row)))
+                return False
+            except Exception, e:
+                logger.error(str(e))
+                logger.error("%s error: type of %s(type = %s) not equal to %s" % (str(row), str(element), type(element), self.typeList[i]))
+                return False
+                
+        return True
+
+
+def getLog():
+    logging.basicConfig(filename='questionGetter.log',
+                        filemode='w',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s :\n%(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.WARNING)
+    return logging.getLogger('QuestionGetter')
+        
+if __name__ == "__main__":
+    logger = getLog()
+    qd = QuestionDownloader('googleDocsRetrieveTest-9f78e34059f7.json', "https://docs.google.com/spreadsheets/d/13Y8EbTCLtuBJYQyQ_eXwveac3diD4ToGXpUsMfVSgHw/edit?usp=sharing")
+    #qd.SaveAllSheetsinJSON()
+    qd.SaveSheetinJSON(0)
     
-writer.writerows(valueListWithoutTitle)
-f.close()
 
-# Transform CSV to json
-fieldnames = valueList[0][:]
-f=open("Questions.csv", 'r')
-csv_reader = csv.DictReader(f,fieldnames)
-json_filename = "Questions.json"
-jsonf = open(json_filename,'w')
-data = json.dumps([r for r in csv_reader], indent=4, separators=(',', ': '))
-jsonf.write(data)
-f.close()
-jsonf.close()
